76b39f278c3a74edbc2e36fec3a1b9574b72289f
sparkles-laurel <sparkles@getconfigured.org>
2026-04-01 10:59:43 +0300

    chore: remove debug log, add license file, and update test environment configuration

--- qemu-testing/runtest.sh
+++ qemu-testing/runtest.sh
@@ -7,6 +7,7 @@ cd $(dirname $0)
 exec qemu-system-x86_64 -enable-kvm -machine q35 \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd \
     -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd \
-    -drive format=raw,file=fat:rw:esp
+    -drive format=raw,file=fat:rw:esp \
+    -serial stdio
 
 popd
